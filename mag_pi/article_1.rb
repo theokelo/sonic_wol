cd2b5db2f9be677713bb3871f17d439b5c885a33
Justin <justin.worrall@gmail.com>
2016-08-20 16:53:25 +0100

    added more articles

---
+++ mag_pi/article_1.rb
@@ -0,0 +1,9 @@
+live_loop :rand_surfer do
+  use_random_seed 1
+  use_synth :dsaw
+  notes = (scale :e2, :minor_pentatonic, num_octaves: 2)
+  16.times do
+    play notes.choose, release: 0.1, cutoff: rrand(70, 120)
+    sleep 0.125
+  end
+end
