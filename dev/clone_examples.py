29b4c8c0520794df351c43354e88ed08616d488f
Justin <justin.worrall@gmail.com>
2016-08-21 08:00:24 +0100

    renamed copy as clone

--- dev/clone_examples.py
+++ dev/clone_examples.py
@@ -1,23 +1,23 @@
 """
-- script to copy all examples
+- script to clone all examples
 """
 
 import os, re
 
-def copy(srcdir, destdir):
-    def copy(currentdir):
+def clone(srcdir, destdir):
+    def clone(currentdir):
         for item in os.listdir(currentdir):
             newitem=currentdir+"/"+item
             if os.path.isdir(newitem):
                 print "[dir] %s" % item
-                copy(newitem)
+                clone(newitem)
             else:
                 print "[file] %s" % item                            
                 text=file(newitem).read()
                 dest=file(destdir+"/"+item, 'w')
                 dest.write(text)
                 dest.close()
-    copy(srcdir)
+    clone(srcdir)
 
 if __name__=="__main__":
-    copy("../sonic-pi-master/etc/examples", "./demos/sonic_pi_examples")
+    clone("../sonic-pi-master/etc/examples", "./demos/sonic_pi_examples")
