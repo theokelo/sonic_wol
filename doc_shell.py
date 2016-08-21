4e857c995490c85afc5bac74fc4219638968a7b7
Justin <justin.worrall@gmail.com>
2016-08-21 09:27:27 +0100

    added doc shell

---
+++ doc_shell.py
@@ -0,0 +1,36 @@
+import cmd
+
+class DocShell(cmd.Cmd):
+    
+    prompt=">>> "
+
+    def __init__(self):
+        cmd.Cmd.__init__(self)
+
+    # start helpers
+
+    def emptyline(self):
+        pass
+
+    def preloop(self):
+        print "Welcome to Sonic-Pi docs!"
+        pass
+
+    # helpers
+
+    def do_hello(self, line):
+        print "Hello World!"
+
+    # end helpers
+
+    def do_exit(self, line): 
+        return True
+
+    def do_quit(self, line):
+        return True
+
+    def postloop(self):
+        pass
+
+if __name__=="__main__":
+    DocShell().cmdloop()
