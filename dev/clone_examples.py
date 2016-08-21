d008b74d12e510958b7573dff825a441ed2399ef
Justin <justin.worrall@gmail.com>
2016-08-21 07:59:54 +0100

    updated clone_examples

--- dev/clone_examples.py
+++ dev/clone_examples.py
@@ -1,5 +1,5 @@
 """
-- script to run iterate through Sonic Pi /src/examples and copy all files containig 'Sam Aaron'
+- script to copy all examples
 """
 
 import os, re
@@ -7,17 +7,17 @@ import os, re
 def copy(srcdir, destdir):
     def copy(currentdir):
         for item in os.listdir(currentdir):
-            # print currentdir+"/"+item
-            if os.path.isdir(currentdir+"/"+item):
+            newitem=currentdir+"/"+item
+            if os.path.isdir(newitem):
                 print "[dir] %s" % item
-                copy(currentdir+"/"+item)
+                copy(newitem)
             else:
                 print "[file] %s" % item                            
-                text=file(currentdir+"/"+item).read()
+                text=file(newitem).read()
                 dest=file(destdir+"/"+item, 'w')
                 dest.write(text)
                 dest.close()
     copy(srcdir)
 
 if __name__=="__main__":
-    copy("../sonic-pi-master/etc/examples", "./sonic_pi_examples")
+    copy("../sonic-pi-master/etc/examples", "./demos/sonic_pi_examples")
