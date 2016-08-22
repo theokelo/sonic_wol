fdfb7b22b17439c343bc2ee28b910fcabd65cfdc
Justin <justin.worrall@gmail.com>
2016-08-22 09:38:47 +0100

    added synthdefs

--- dev/parse_stealthissound.py
+++ dev/parse_stealthissound.py
@@ -1,10 +1,38 @@
+# https://github.com/supercollider/supercollider/blob/master/examples/demonstrations/stealthissound.scd
+
 import re
 
+def tokenize(text):
+    def tokenize(text):
+        return [tok for tok in text.split("\n")
+                if (tok!='' and
+                    not re.sub("\\s", "", tok).startswith("//"))]
+    groups=[[]]
+    for token in tokenize(text):
+        if token in ['(', ')']:
+            groups.append([])
+        else:
+            groups[-1].append(token)
+            if re.search("\\.((add)|(play))\\;?$", token):
+                groups.append([])
+    return ["\n".join(group)
+            for group in groups
+            if group!=[]]
+
+def parse(srcfile, destdir):
+    text=file(srcfile).read()
+    for token in tokenize(text):
+        if not token.startswith("SynthDef"):
+            continue
+        args=[tok for tok in re.split("\\W", token.split("\n")[0])
+              if tok!='']
+        print args[1]
+        dest=file("%s/%s.scd" % (destdir, args[1]), 'w')
+        dest.write(re.sub("\\.((add)|(play))\\;", "", token))
+        dest.close()
+
 if __name__=="__main__":
-    text=file("dev/stealthissound.scd").read()
-    tokens=[tok for tok in text.split("\n")
-            if (tok!='' and
-                not re.sub("\\s", "", tok).startswith("//"))]
-    dest=file("tmp/stealthissound.scd", 'w')
-    dest.write("\n".join(tokens))
-    dest.close()
+    parse("./dev/stealthissound.scd", "synthdefs/stealthissound")
+    
+            
+
