2ec76a483a5dc2ab1e082c74c2a54470441523f4
Justin <justin.worrall@gmail.com>
2016-08-22 11:08:06 +0100

    added basic synth parsinG

--- dev/parse_synths.py
+++ dev/parse_synths.py
@@ -4,6 +4,19 @@
 
 # https://github.com/samaaron/sonic-pi/blob/master/etc/doc/cheatsheets/synths.md
 
+import re
+
 if __name__=="__main__":
     text=file("dev/synths.md").read()    
-    print text
+    rows=text.split("\n")
+    for i, row in enumerate(rows):
+        if row=="### Key:":
+            name=re.sub("\\s", "", rows[i+1])[1:]
+            print "name: %s" % name
+        elif row=="### Doc:":
+            desc=" ".join([tok for tok in re.split("\\s", rows[i+1])
+                           if tok!=''])
+            print "desc: %s" % desc
+        elif row=="### Opts:":
+            print "[opts]"
+
