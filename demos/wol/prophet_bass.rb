a6ccde88d0c611a68bf56060ee04817e0f74391b
Justin <justin.worrall@gmail.com>
2016-08-28 09:29:35 +0100

    updated prophet bass

--- demos/wol/prophet_bass.rb
+++ demos/wol/prophet_bass.rb
@@ -1,16 +1,11 @@
 live_loop :bass do
   use_random_seed 13
-  8.times do     
-    n = (rand < 0.9) ? :e1 : :e2
-    a = (rand < 0.8) ? 1 : 1.2
+  32.times do    
+    n = (rand < 0.8) ? :e1 : :e2
+    a = (rand < 0.8) ? 0.75 : 1.5
     s = synth :prophet, note: n, amp: a, cutoff: rrand(70, 90), cutoff_slide: 0.2 if rand < 0.9
-    control s, cutoff: rrand(30, 60)
+    control s, cutoff: rrand(30, 50)
     sleep 0.125
   end
-end
-
-live_loop :beat do 
-  sample :bd_haus, amp: 0.1
-  sleep 0.5
 end
 
